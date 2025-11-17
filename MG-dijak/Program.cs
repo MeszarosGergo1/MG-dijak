6df6a93ecbf4b19db400bbdb2a960742798bca3f
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-11-17 14:21:51 +0100

    asd

--- MG-dijak/Program.cs
+++ MG-dijak/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace MG_dijak
            MG-díjak
            MG 2025.11.17.
            */
-            string fejlec = "Díjak";
+            string fejlec = "Baseball díjak 2007 - 2010";
             Console.WriteLine(fejlec);
 
             for (int i = 0; i < fejlec.Length; i++)
@@ -24,8 +25,20 @@ namespace MG_dijak
             }
             Console.WriteLine();
 
+            List<List<string>> dijak = new List<List<string>>();
+            
+            StreamReader sr = new StreamReader("../../dijak.csv", Encoding.UTF8);
+
+            string[] asd = sr.ReadLine().Split(';');
+            
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split(';');
+                dijak.Add(sor.ToList());
+            }
 
 
+            Console.WriteLine();
 
             Console.WriteLine();
             Console.WriteLine("Nyomja meg az ENTER-t a kilépéshez");
