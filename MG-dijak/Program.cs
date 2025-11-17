af698adf7346120c656a2eccec9ff45c766498bf
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-11-17 14:28:05 +0100

    asd

--- MG-dijak/Program.cs
+++ MG-dijak/Program.cs
@@ -37,8 +37,26 @@ namespace MG_dijak
                 dijak.Add(sor.ToList());
             }
 
+            Console.WriteLine($"3. feladat:\n\t2007 és 2010 között {dijak.Count} díjat osztottak ki.");
+            Console.Write($"4. feladat:\n\tKérem egy játékos nevét:");
+            string nev = Console.ReadLine();
 
-            Console.WriteLine();
+            if (dijak.Any(x => x[0] == nev))
+            {
+                for (int i = 0; i < dijak.Count; i++)
+                {
+                    if (dijak[i][0] == nev)
+                    {
+                        Console.WriteLine($"név: {dijak[i][0]}");
+                        Console.WriteLine($"díj: {dijak[i][1]}");
+                        Console.WriteLine($"év: {dijak[i][2]}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"\tNincs ilyen nevű jűtékos!");
+            }
 
             Console.WriteLine();
             Console.WriteLine("Nyomja meg az ENTER-t a kilépéshez");
