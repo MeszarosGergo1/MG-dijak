0ad978fbb564e6ba928feac3c30dfc1f4b550940
MeszarosGergo1 <meszaros.gergo@diak.szbi-pg.hu>
2025-11-17 14:33:45 +0100

    asdasd

--- MG-dijak/Program.cs
+++ MG-dijak/Program.cs
@@ -38,24 +38,31 @@ namespace MG_dijak
             }
 
             Console.WriteLine($"3. feladat:\n\t2007 és 2010 között {dijak.Count} díjat osztottak ki.");
-            Console.Write($"4. feladat:\n\tKérem egy játékos nevét:");
+            Console.Write($"4. feladat:\n\tKérem egy játékos nevét: ");
             string nev = Console.ReadLine();
-
+            Console.WriteLine("5. feladat:");
             if (dijak.Any(x => x[0] == nev))
             {
                 for (int i = 0; i < dijak.Count; i++)
                 {
                     if (dijak[i][0] == nev)
                     {
-                        Console.WriteLine($"név: {dijak[i][0]}");
-                        Console.WriteLine($"díj: {dijak[i][1]}");
-                        Console.WriteLine($"év: {dijak[i][2]}");
+                        Console.WriteLine($"\tév: {dijak[i][0]}");
+                        Console.WriteLine($"\tdíj: {dijak[i][1]}");
+                        Console.WriteLine($"\tév: {dijak[i][2]}");
                     }
                 }
             }
             else
             {
                 Console.WriteLine($"\tNincs ilyen nevű jűtékos!");
+            }
+
+            Console.WriteLine("6. feladat:");
+            for (int i = 0; i < dijak.Count; i++)
+            {
+                if (dijak[i][2] == "2009" && dijak[i][1] == "Gold Glove")
+                    Console.WriteLine("\t" +dijak[i][0]);   
             }
 
             Console.WriteLine();
